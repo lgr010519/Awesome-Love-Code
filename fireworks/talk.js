fec80cd64428b3b875c51f17a837796b2b43012b
liugaorong <a15921468768@icloud.com>
2024-07-06 19:14:27 +0800

    fix: 删除多余文件

---
+++ fireworks/talk.js
@@ -0,0 +1,40 @@
+$(function() {
+    $('#yes').click(function(event) {
+        modal('我也是~(^_^)请你看烟花嘿嘿', function() {
+            $('.page_one').addClass('hide');
+            $('.page_two').removeClass('hide');
+            // typeWrite();
+            fireworks();
+
+        });
+    });
+    $('#no').click(function(event) {
+        modal('好吧那我有点难过', A);
+    });
+});
+
+function A() {
+    modal('不过烟花还是得请你看', function() {
+        fireworks();
+    });
+}
+
+function fireworks() {
+    $('.page_one').addClass('hide');
+    initAnimate();
+}
+
+function modal(content, callback) {
+    var tpl = '<div class="container">'+
+        '<div class="mask"></div>'+
+        '<div class="modal">'+
+        '<p>'+ content +'</p>'+
+        '<button type="button" id="confirm" class="confirm">确定</button>'+
+        '</div>'+
+        '</div>';
+    $('body').append(tpl);
+    $(document).on('click', '.confirm', function() {
+        $('.container').remove();
+        callback();
+    });
+}
